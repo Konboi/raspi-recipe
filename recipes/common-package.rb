7474672e7fa0589278d74a45519a29a33b1644cb
Konboi <ryosuke.yabuki@gmail.com>
2015-10-01 23:55:22 +0900

    修正

--- recipes/common-package.rb
+++ recipes/common-package.rb
@@ -6,8 +6,5 @@ package "gettext"
 package "libghc-zlib-dev"
 package "wget"
 package "gcc"
-
-execute "install new git version" do
-  not_if "which git"
-  command "cd /tmp && wget https://www.kernel.org/pub/software/scm/git/git-2.4.8.tar.gz && tar -zxvf git-2.4.8.tar.gz && cd git-2.4.8/ && ./configure --prefix=/usr/local --with-curl --with-expat && sudo make && sudo make install"
-end
+package "dnsutils"
+package "git"
