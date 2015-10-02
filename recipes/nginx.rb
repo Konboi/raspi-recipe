5edf57eeb770b72ee1f86073d139832fb1b6ac11
Konboi <ryosuke.yabuki@gmail.com>
2015-10-02 12:51:10 +0900

    nginx.confをtemplateに出した

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -3,3 +3,15 @@ package "nginx"
 service "nginx" do
   action [:enable, :start]
 end
+
+worker_processes   = 1
+worker_connections = 1024
+
+template "/etc/nginx/nginx.conf" do
+  source "../templates/etc/nginx/nginx.conf.erb"
+  notifies :reload, "service[nginx]"
+  variables(
+    worker_processes:  worker_processes,
+    worker_connections: worker_connections
+  )
+end
